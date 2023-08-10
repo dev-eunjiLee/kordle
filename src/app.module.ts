ea2e28d4faeff50f72137c8f169235ee20b267e5
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-10 23:48:33 +0900

    FIX: (app) kwordle 모듈 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KwordleModule } from './kwordle/kwordle.module';
 
 @Module({
-  imports: [],
+  imports: [KwordleModule],
   controllers: [AppController],
   providers: [AppService],
 })
