eea2388ed07ddfa200113e82912b45e3756a81f6
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-10 23:48:10 +0900

    FIX: (kwordle) controller 셋팅만

--- src/kwordle/kwordle.controller.ts
+++ src/kwordle/kwordle.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('kwordle')
-export class KwordleController {}
+export class KwordleController {
+  @Get('insert/:keyword')
+  insert(@Param('keyword') keyword: string) {
+    console.log(keyword);
+  }
+}
