7a3c9d7f2f6491563bef89df04faa6dad51a5cb8
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-11 00:10:51 +0900

    FIX: (kwordle) validation 작업중

--- src/kwordle/kwordle.controller.ts
+++ src/kwordle/kwordle.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { IsString } from 'class-validator';
+
+export class InsertedKeywordValidation {
+  // TODO 숫자 string은 제외하도록
+  // TODO validation 클래스 파일 분리
+  @IsString()
+  keyword: string;
+}
 
 @Controller('kwordle')
 export class KwordleController {
   @Get('insert/:keyword')
-  insert(@Param('keyword') keyword: string) {
-    console.log(keyword);
+  insert(@Param() params: InsertedKeywordValidation) {
+    console.log(params);
   }
 }
