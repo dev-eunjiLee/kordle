b54ea6a82877cfbde7ef3a99899b60d438a5debf
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-15 00:01:58 +0900

    FIX: (app) 필터 적용

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ValidationExceptionFilter } from './common/error/exceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new ValidationExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
