bf0a5b0f6d8e15ae9527fc69700694fa45317cfa
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-15 01:20:37 +0900

    FIX: (kwordle) 답변 처리 함수 셋팅만

--- src/kwordle/kwordle.service.ts
+++ src/kwordle/kwordle.service.ts
@@ -12,6 +12,14 @@ export class KwordleService implements OnModuleInit {
     return this.todayKeyword.getKeyword();
   }
 
+  answer(keyword: string) {
+    const correctAnswer = this.getKeyword();
+
+    // TODO 키워드 자모음 분리 후 키워드와 비교
+
+    return correctAnswer === keyword;
+  }
+
   onModuleInit() {
     // TODO 랜덤 키워드 셋팅되도록 작업 필요
     this.todayKeyword.setKeyword('ㅎㅏㄴㅇㅜ');
