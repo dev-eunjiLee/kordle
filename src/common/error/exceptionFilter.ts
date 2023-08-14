a4d8986436b25a96fed75e8920330502e38f7490
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-15 00:01:39 +0900

    FIX: (error) validationError 셋팅

---
+++ src/common/error/exceptionFilter.ts
@@ -0,0 +1,21 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class ValidationExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    console.log(exception);
+
+    response.json({
+      timestamp: new Date().toISOString(),
+      path: request.route.path,
+      message: exception.message,
+      params: request.params,
+      stack: `${exception.stack.slice(0, 50)}...`,
+    });
+  }
+}
