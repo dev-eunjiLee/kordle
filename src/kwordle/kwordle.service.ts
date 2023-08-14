e1b0c24c469c05d58f4cee0d8b9003ecc3e4e90e
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-15 01:08:06 +0900

    FIX: (kwordle) 앱 띄울 때, 키워드 셋팅(현재는 임시 키워드)

--- src/kwordle/kwordle.service.ts
+++ src/kwordle/kwordle.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { TodayKeyword } from './types/TodayKeyword.class';
 
 @Injectable()
-export class KwordleService {}
+export class KwordleService implements OnModuleInit {
+  private readonly todayKeyword: TodayKeyword;
+  constructor() {
+    this.todayKeyword = new TodayKeyword();
+  }
+
+  getKeyword(): string {
+    return this.todayKeyword.getKeyword();
+  }
+
+  onModuleInit() {
+    // TODO 랜덤 키워드 셋팅되도록 작업 필요
+    this.todayKeyword.setKeyword('ㅎㅏㄴㅇㅜ');
+  }
+}
