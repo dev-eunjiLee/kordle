b0d84d53f0dbb216b9527aa38f341f0db6232c35
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-19 18:05:14 +0900

    FIX: (kwordle) 정답 확인 로직 완성

--- src/kwordle/kwordle.service.ts
+++ src/kwordle/kwordle.service.ts
@@ -11,17 +11,39 @@ export class KwordleService implements OnModuleInit {
   getKeyword(): string {
     return this.todayKeyword.getKeyword();
   }
+  getKeywordList(): Array<string> {
+    return this.todayKeyword.getKeywordList();
+  }
 
-  answer(keyword: string) {
-    const correctAnswer = this.getKeyword();
+  answer(keyword: Array<string>): Array<Array<string>> {
+    console.log(keyword);
 
-    // TODO 키워드 자모음 분리 후 키워드와 비교
+    const answerList = this._checkAnswer(keyword);
 
-    return correctAnswer === keyword;
+    return [keyword, answerList];
+  }
+
+  private _checkAnswer(keyword: Array<string>): Array<string> {
+    const correctAnswerList = this.getKeywordList();
+    const answer: Array<'O' | 'X' | 'ㅁ'> = [];
+
+    keyword.forEach((per, i) => {
+      if (correctAnswerList[i] === per) {
+        answer.push('O');
+      } else {
+        if (correctAnswerList.includes(per)) {
+          answer.push('ㅁ');
+        } else {
+          answer.push('ㅁ');
+        }
+      }
+    });
+
+    return answer;
   }
 
   onModuleInit() {
     // TODO 랜덤 키워드 셋팅되도록 작업 필요
-    this.todayKeyword.setKeyword('ㅎㅏㄴㅇㅜ');
+    this.todayKeyword.setKeywordAndList('한우');
   }
 }
