9c97ae323149d7dfa926df17571fe2bff0fcb6bf
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-19 15:24:09 +0900

    FIX: (앱) 한글 모듈 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HangulModule } from './common/hangul/hangul.module';
 import { KwordleModule } from './kwordle/kwordle.module';
 
 @Module({
-  imports: [KwordleModule],
+  imports: [KwordleModule, HangulModule],
   controllers: [AppController],
   providers: [AppService],
 })
