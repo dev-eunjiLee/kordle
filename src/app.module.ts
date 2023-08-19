da40f2b44d72dd231ffaa9c0163465abb04c4e02
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-19 17:24:06 +0900

    FIX: (App) 모듈 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HangulModule } from './common/hangul/hangul.module';
 import { KwordleModule } from './kwordle/kwordle.module';
 
 @Module({
-  imports: [KwordleModule, HangulModule],
+  imports: [KwordleModule],
   controllers: [AppController],
   providers: [AppService],
 })
