ceb4811f35e25acac292bb7afc7223acbd34c8e6
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-08-19 15:30:42 +0900

    FIX: (한글) 각 글자별 한글 체크 함수를 키워드 한글 체크용 함수에 적용

--- src/common/hangul/hangul.service.ts
+++ src/common/hangul/hangul.service.ts
@@ -8,14 +8,29 @@ import {
 export class HangulService {
   constructor(
     @Inject(HANGUL_START_CHARCODE_TOKEN)
-    private readonly hangulStartCharCode: string,
+    private readonly hangulStartCharCode: number,
     @Inject(HANGUL_END_CHARCODE_TOKEN)
-    private readonly hangulEndCharCode: string,
+    private readonly hangulEndCharCode: number,
   ) {
-    this.isHangul();
+    console.log(this.isHangul('가123'));
   }
-  isHangul() {
-    console.log(this.hangulStartCharCode);
-    console.log(this.hangulEndCharCode);
+
+  isHangul(word: string): boolean {
+    let isHangul = true;
+    for (const per of word) {
+      const flag = this._perIsHangul(per);
+      if (flag === false) {
+        isHangul = false;
+        break;
+      }
+    }
+    return isHangul;
+  }
+
+  private _perIsHangul(char: string): boolean {
+    const charCode = char.charCodeAt(0);
+    return (
+      this.hangulStartCharCode <= charCode && charCode <= this.hangulEndCharCode
+    );
   }
 }
