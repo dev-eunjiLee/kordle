b384ed39eda3928cba4116dd7b3ee76fbee8c5b6
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-11-27 00:12:55 +0900

    FIX: (kwordle) 답안제출 input dto transform 작업중

--- src/kwordle/dtos/submitAnswer.dto.ts
+++ src/kwordle/dtos/submitAnswer.dto.ts
@@ -1,9 +1,18 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { Transform, TransformFnParams } from 'class-transformer';
 import { IsString } from 'class-validator';
+import * as HANGUL from 'hangul-js';
+
+function disassembleString({ value }: TransformFnParams) {
+  const disassembledList = HANGUL.disassemble(value);
+  console.log(disassembledList);
+  return disassembledList;
+}
 
 @InputType()
 export class SubmitAnswerInputDto {
   @Field(() => String, { description: '유저가 입력한 답안' })
   @IsString()
+  @Transform(disassembleString)
   answer: string;
 }
