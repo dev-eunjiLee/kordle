1d67130c702aebd3d067ae41864683b80df764d1
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-11-26 23:00:17 +0900

    FIX: (kwordle) 서비스 임시 등록

---
+++ src/kwordle/kwordle.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class KwordleService {
+  submitAnswer() {
+    return 'hi';
+  }
+
+  getCorrectAnswer() {
+    return 'hi';
+  }
+}
