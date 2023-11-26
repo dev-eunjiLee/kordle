4c54013e8b185aea3c757c784471a6c84c8666d1
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-11-26 21:57:42 +0900

    FIX: (main.ts) 예전 코드 정리

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-s  await app.listen(3000);
+  await app.listen(3000);
 }
 bootstrap();
