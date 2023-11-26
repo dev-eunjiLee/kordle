b50304cce282a85b153bca3cab6623d57bbb3774
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-11-26 23:00:06 +0900

    FIX: (kwordle) 서비스 적용

--- src/kwordle/kwordle.resolver.ts
+++ src/kwordle/kwordle.resolver.ts
@@ -1,14 +1,16 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { KwordleService } from './kwordle.service';
 
-@Resolver()
+@Resolver(() => KwordleResolver)
 export class KwordleResolver {
+  constructor(private readonly kwordleService: KwordleService) {}
   @Query(() => String, { nullable: true, description: '쿼들 답안 제출' })
   submitAnswer() {
-    return 'hi';
+    return this.kwordleService.submitAnswer();
   }
 
   @Query(() => String, { nullable: true, description: '쿼들 정답 확인' })
   getCorrectAnswer() {
-    return 'hi';
+    return this.kwordleService.getCorrectAnswer();
   }
 }
