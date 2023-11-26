8b9561d019895636deb9165d5b0f0e31c5ac1915
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-11-26 22:16:33 +0900

    FIX: (APP) graphql 기본 셋팅(driver, autoSchema, useGlobalPrefix)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KwordleModule } from './kwordle/kwordle.module';
 
 @Module({
-  imports: [],
+  imports: [
+    /**
+     * util
+     */
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      useGlobalPrefix: true,
+    }),
+    /**
+     * 실제 서비스 로직
+     */
+    KwordleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
