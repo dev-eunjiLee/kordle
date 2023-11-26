74406ac861ddbc12ffc9be6f36e8ada9e0824e2b
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-11-26 22:59:51 +0900

    FIX: (kwordle) 서비스 등록

--- src/kwordle/kwordle.module.ts
+++ src/kwordle/kwordle.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { KwordleResolver } from './kwordle.resolver';
+import { KwordleService } from './kwordle.service';
 
 @Module({
-  providers: [KwordleResolver],
+  providers: [KwordleResolver, KwordleService],
 })
 export class KwordleModule {}
