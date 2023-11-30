56d160d36ce0f69bbe876b0d0fedf1cd0e4804b8
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-11-30 23:44:04 +0900

    FIX: (kwordle) 기존 dto 걷어내기

--- src/kwordle/dtos/submitAnswer.dto.ts
+++ src/kwordle/dtos/submitAnswer.dto.ts
@@ -1,25 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { Transform, TransformFnParams } from 'class-transformer';
-import { ArrayMaxSize, ArrayMinSize, IsArray, IsString } from 'class-validator';
-import * as HANGUL from 'hangul-js';
-
-function DisassembleStringByHangul({ value }: TransformFnParams) {
-  let disassembledList: Array<string | number> = [];
-  disassembledList = HANGUL.disassemble(value).map((char: string) => {
-    if (isNaN(Number(char))) return char;
-    else return Number(char);
-  });
-  console.log(disassembledList);
-  return disassembledList;
-}
+import { IsString } from 'class-validator';
 
 @InputType()
 export class SubmitAnswerInputDto {
   @Field(() => String, { description: '유저가 입력한 답안' })
-  @Transform(DisassembleStringByHangul)
-  @IsString({ each: true })
-  @IsArray()
-  @ArrayMinSize(0)
-  @ArrayMaxSize(5)
-  answer: string[];
+  @IsString()
+  answer: string;
 }
