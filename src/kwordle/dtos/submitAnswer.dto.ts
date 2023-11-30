8f7e06020eb20241f68ba1bbbd3aafcd5390219b
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-01 00:13:36 +0900

    FIX: (kwordle) 정답 제출 아웃풋 dto 셋팅

--- src/kwordle/dtos/submitAnswer.dto.ts
+++ src/kwordle/dtos/submitAnswer.dto.ts
@@ -1,4 +1,4 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { IsString } from 'class-validator';
 
 @InputType()
@@ -6,4 +6,13 @@ export class SubmitAnswerInputDto {
   @Field(() => String, { description: '유저가 입력한 답안' })
   @IsString()
   answer: string;
+}
+
+@ObjectType()
+export class SubmitAnswerOutputDto {
+  @Field(() => Boolean, { description: '정답 여부' })
+  correctFlag: boolean;
+
+  @Field(() => [String], { description: '글자 위치별 일치 여부' })
+  correctList: string[];
 }
