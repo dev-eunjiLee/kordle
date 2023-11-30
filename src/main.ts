5c3f50847ded06bbf8cfa59ce5b25f5c0e871f69
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-11-30 23:09:30 +0900

    FIX: (main) ValidationPipe가 자동으로 데이터 교정할 수 있도록 옵션 설정(transform: true)

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /**
+       * ValidationPipe로 자동으로 데이터 교정하기
+       * 참고 링크: https://docs.nestjs.com/techniques/validation#transform-payload-objects
+       */
+      transform: true, //
+    }),
+  );
   app.setGlobalPrefix('v1');
   await app.listen(3000);
 }
