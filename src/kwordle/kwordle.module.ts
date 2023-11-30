5fa814232dc5d94b82e8caf4c3ab49ee7fe7a38a
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-01 00:00:57 +0900

    FIX: (kwordle) 모듈 셋팅

--- src/kwordle/kwordle.module.ts
+++ src/kwordle/kwordle.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { KwordleResolver } from './kwordle.resolver';
 import { KwordleService } from './kwordle.service';
+import { PreKwordleService } from './pre.kwordle.service';
 
 @Module({
-  providers: [KwordleResolver, KwordleService],
+  providers: [
+    /**
+     * Resolvers
+     */
+    KwordleResolver,
+    /**
+     * Services
+     */
+    KwordleService,
+    PreKwordleService,
+  ],
 })
 export class KwordleModule {}
