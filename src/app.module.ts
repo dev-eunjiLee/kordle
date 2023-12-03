c667ea3f7f96990a60b1050d30f5b233421cd00f
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-03 22:50:25 +0900

    ADD: (App) ConfigModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KwordleModule } from './kwordle/kwordle.module';
@@ -14,6 +16,21 @@ import { KwordleModule } from './kwordle/kwordle.module';
       driver: ApolloDriver,
       autoSchemaFile: true,
       useGlobalPrefix: true,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DICTIONARY_API_KEY: Joi.string().required(),
+      }),
+      validationOptions: {
+        /**
+         * abortEarly
+         * - true: (Default) 환경변수를 확인하다가 첫번째 에러를 발견하면 그 즉시 에러를 리턴
+         * - false: 환경변수 확인 중 에러가 발생하더라도 전체를 다 확인한 후 모아서 에러를 리턴
+         */
+        abortEarly: false, // false인 경우 validationSchema에 지정된 모든 키를 다 확인한 후 모아서 에러를 준다
+      },
     }),
     /**
      * 실제 서비스 로직
