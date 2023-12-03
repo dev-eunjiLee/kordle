0e2cea5c794934b2dec464d836ad9757c49a3a49
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-03 20:31:37 +0900

    FIX: (kwordle) 한글 분리하는 로직 내 코드 간소화 및 타입 지정

--- src/common/disassembleStringByHangul.ts
+++ src/common/disassembleStringByHangul.ts
@@ -1,10 +1,8 @@
 import * as hangul from 'hangul-js';
 
-export function DisassembleStringByHangul(keyword: string) {
-  let disassembledList: Array<string | number> = [];
-  disassembledList = hangul.disassemble(keyword).map((char: string) => {
+export function DisassembleStringByHangul(keyword: string): Array<string> {
+  return hangul.disassemble(keyword).map((char: string) => {
     if (isNaN(Number(char))) return char;
     else throw Error(`키워드를 다시 확인해주세요 (사유: 숫자 포함)`);
   });
-  return disassembledList;
 }
