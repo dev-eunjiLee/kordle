4420ba66ce37d4d50a829865ad03394e96b92f18
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-11 00:06:57 +0900

    FIX: esm 모듈 호출(got)

--- src/util/web-client/classes/got-client.ts
+++ src/util/web-client/classes/got-client.ts
@@ -1,5 +1,8 @@
 import { WebClient } from '../interfaces/web-client.interface';
-import got from 'got';
+/**
+ * [모듈 호출 방식 참고 방식 공부해서 정리한 링크(ESM)](https://just-dev-poi.tistory.com/54)
+ */
+const got = import('got');
 
 export class GotClient implements WebClient {
   get(): this {
