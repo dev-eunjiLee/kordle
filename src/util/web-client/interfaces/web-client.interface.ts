0c1816e63b460d68d6f2f1e414d1a601cf841d8f
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-18 00:05:37 +0900

    FIX: (WebClient) retrieve 함수 추가, property에 option 추가, method 함수로 변경, HTTP_METHOD 타입 지정

--- src/util/web-client/interfaces/web-client.interface.ts
+++ src/util/web-client/interfaces/web-client.interface.ts
@@ -1,14 +1,22 @@
+import { HTTP_METHOD } from '../types/method.type';
+
 // 참고문서: https://tech.inflab.com/20230723-pure-http-client/
-// TODO Start here
 export interface WebClient {
+  option: any;
+
   /**
    * HTTP METHOD
    */
-  get(): this;
+  method(method: HTTP_METHOD): this;
 
   /**
    * HTTP 요청을 위한 정보 설정
    */
   header(param: Record<string, string>): this;
   body(data: Record<string, unknown>): this;
+
+  /**
+   * builder 패턴으로 만들어진 WebClient를 이용해 request를 날리기
+   */
+  retrieve(): Promise<any>;
 }
