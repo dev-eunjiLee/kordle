a698f4ecdce7aea9ce803135d6d8481d839c49a2
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-21 23:11:11 +0900

    FIX: (web-client) 모듈에 axiosClientService 등록

--- src/util/web-client/web-client.module.ts
+++ src/util/web-client/web-client.module.ts
@@ -1,5 +1,14 @@
 import { Global, Module } from '@nestjs/common';
+import { AxiosClientService, WebClientService } from './web-client.service';
 
 @Global()
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: WebClientService,
+      useClass: AxiosClientService,
+    },
+  ],
+  exports: [WebClientService],
+})
 export class WebClientModule {}
