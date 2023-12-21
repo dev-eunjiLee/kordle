44bf5a8cb3ef20ed4eaf8f28b33fc985fa229461
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-21 23:35:53 +0900

    FIX: (axios-client) 필요없는 함수 제거, params 함수 구현 추가, Retrieve 구현 수정중

--- src/util/web-client/classes/axios-client.ts
+++ src/util/web-client/classes/axios-client.ts
@@ -3,9 +3,6 @@ import { WebClient } from '../interfaces/web-client.interface';
 import { HTTP_METHOD } from '../types/method.type';
 
 export class AxiosClient extends WebClient {
-  url(url: string): this {
-    throw new Error('Method not implemented.');
-  }
   method(method: HTTP_METHOD): this {
     this.option.method = method;
     return this;
@@ -15,6 +12,10 @@ export class AxiosClient extends WebClient {
   }
   body(data: Record<string, unknown>): this {
     throw new Error('Method not implemented.');
+  }
+  params(params: Record<string, unknown>): this {
+    this.option.params = params;
+    return this;
   }
 
   // 필수 키 체크
@@ -35,6 +36,11 @@ export class AxiosClient extends WebClient {
   // builder 패턴으로 만들어진 Client를 이용해 최종적으로 Http Request를 날려 요청을 받는다
   async retrieve(): Promise<any> {
     const insertedOptionKeyList = Object.keys(this.option);
-    throw new Error('Method not implemented.');
+    console.log(this.option);
+    const result = await axios({
+      ...this.option,
+    });
+    console.log(result);
+    return result;
   }
 }
