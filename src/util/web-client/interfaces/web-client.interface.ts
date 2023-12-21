6e2c478ac22fd90cc39f6bdb76c3f2e1ed1f4bc5
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-21 23:13:18 +0900

    FIX: (web-client) constructor를 사용하기위해 추상 클래스로 변경

--- src/util/web-client/interfaces/web-client.interface.ts
+++ src/util/web-client/interfaces/web-client.interface.ts
@@ -1,22 +1,27 @@
 import { HTTP_METHOD } from '../types/method.type';
 
 // 참고문서: https://tech.inflab.com/20230723-pure-http-client/
-export interface WebClient {
-  option: any;
+export abstract class WebClient {
+  constructor(url: string, apiKey?: string) {
+    this.option.url = url;
+    this.option.apiKey = apiKey;
+  }
+
+  protected option: any;
 
   /**
    * HTTP METHOD
    */
-  method(method: HTTP_METHOD): this;
+  abstract method(method: HTTP_METHOD): this;
 
   /**
    * HTTP 요청을 위한 정보 설정
    */
-  header(param: Record<string, string>): this;
-  body(data: Record<string, unknown>): this;
+  abstract header(param: Record<string, string>): this;
+  abstract body(data: Record<string, unknown>): this;
 
   /**
    * builder 패턴으로 만들어진 WebClient를 이용해 request를 날리기
    */
-  retrieve(): Promise<any>;
+  abstract retrieve(): Promise<any>;
 }
