302a35d1d7c122548d85be5b1d4b05dcde262e8f
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-21 23:34:25 +0900

    FIX: (kwordle) 키워드 검색 테스트로 하드코딩으로 속성 다 때려넣음

--- src/kwordle/kwordle.module.ts
+++ src/kwordle/kwordle.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { FIRST_KEYWORD } from 'src/util/consts/module-token.const';
+import { WebClientService } from 'src/util/web-client/web-client.service';
 import { KwordleResolver } from './kwordle.resolver';
 import { KwordleService } from './kwordle.service';
 import { PreKwordleService } from './pre.kwordle.service';
@@ -21,9 +22,26 @@ import { PreKwordleService } from './pre.kwordle.service';
      */
     {
       provide: FIRST_KEYWORD,
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
+      inject: [ConfigService, WebClientService],
+      useFactory: async (
+        configService: ConfigService,
+        webClientService: WebClientService,
+      ) => {
         const apiKey = configService.get('DICTIONARY_API_KEY');
+        // https://stdict.korean.go.kr/api/search.do?certkey_no=6214&key=4AE17465F1242FA5FDCA1FC89340C153&type_search=search&req_type=json&q=%EB%82%98%EB%AC%B4
+
+        await webClientService
+          .create('https://stdict.korean.go.kr/api/search.do')
+          .method('get')
+          .params({
+            certKey_no: configService.get('DICTIONARY_API_KEY'),
+            key: '4AE17465F1242FA5FDCA1FC89340C153',
+            type_search: 'search',
+            req_type: 'json',
+            q: '%EB%82%98%EB%AC%B4',
+          })
+          .retrieve();
+
         return '도레미';
       },
     },
