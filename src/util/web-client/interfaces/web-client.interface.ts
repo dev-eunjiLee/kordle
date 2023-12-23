9768acf690f062f94d631bfa1447f68a570118b9
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-24 00:42:08 +0900

    FIX: (WebClient) 설명정리

--- src/util/web-client/interfaces/web-client.interface.ts
+++ src/util/web-client/interfaces/web-client.interface.ts
@@ -1,30 +1,43 @@
 import { HTTP_METHOD } from '../types/method.type';
 
-// 참고문서: https://tech.inflab.com/20230723-pure-http-client/
+/**
+ * @ref [참고문서](https://tech.inflab.com/20230723-pure-http-client/)
+ * @description 어떤 Http 통신 패키지를 사용하더라도 유연하게 사용할 수 있도록 커스텀한 추상 클래스
+ */
 export abstract class WebClient {
   constructor(url: string) {
-    console.log(url);
     this.option = {
       url: url,
     };
   }
 
-  protected option: any;
+  /**
+   * @description builder 패턴으로 옵션들이 셋팅된다.
+   */
+  protected option: Record<string, any>;
 
   /**
-   * HTTP METHOD
+   * @description HTTP METHOD 셋팅(builder)
    */
   abstract method(method: HTTP_METHOD): this;
 
   /**
-   * HTTP 요청을 위한 정보 설정
+   * @description HTTP 요청을 위한 header 설정(builder)
    */
   abstract header(param: Record<string, string>): this;
+
+  /**
+   * @description HTTP 요청을 위한 body 설정(builder)
+   */
   abstract body(data: Record<string, unknown>): this;
+
+  /**
+   * @description HTTP 요청을 위한 params 설정하는 함수(builder)
+   */
   abstract params(prams: Record<string, unknown>): this;
 
   /**
-   * builder 패턴으로 만들어진 WebClient를 이용해 request를 날리기
+   * @description builder 패턴으로 셋팅된 옵션대로 request를 날리는 함수
    */
   abstract retrieve(): Promise<any>;
 }
