a1cd969ec02dcde44b9e811e3c930bbc5ed95688
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-24 01:08:15 +0900

    FIX: (AxiosClient) Axios 타입 적용

--- src/util/web-client/classes/axios-client.ts
+++ src/util/web-client/classes/axios-client.ts
@@ -1,8 +1,10 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { WebClient } from '../abstract.class/web-client.abstract.class';
 import { HTTP_METHOD } from '../types/method.type';
 
 export class AxiosClient extends WebClient {
+  protected option: AxiosRequestConfig;
+
   method(method: HTTP_METHOD): this {
     this.option.method = method;
     return this;
@@ -18,29 +20,10 @@ export class AxiosClient extends WebClient {
     return this;
   }
 
-  // 필수 키 체크
-  private _checkRequiredKey(insertedOptionKeyList: string[]): boolean {
-    const requiredKeyList = ['method', 'header'];
-    const insertedRequiredKeyList = [];
-    Object.keys(this.option).forEach((key) => {
-      if (requiredKeyList.includes(key)) insertedRequiredKeyList.push(key);
-    });
-    let checkResult = true;
-    if (insertedRequiredKeyList.length !== requiredKeyList.length) {
-      checkResult = false;
-    }
-    return checkResult;
-  }
-
-  // retrieve: 검색하다
-  // builder 패턴으로 만들어진 Client를 이용해 최종적으로 Http Request를 날려 요청을 받는다
-  async retrieve(): Promise<any> {
-    const insertedOptionKeyList = Object.keys(this.option);
-    console.log(this.option);
+  async retrieve(): Promise<AxiosResponse> {
     const result = await axios({
       ...this.option,
     });
-    console.log(result);
     return result;
   }
 }
