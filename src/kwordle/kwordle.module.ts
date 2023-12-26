637ebce1f09f672256a53da6b70e4b028e81f02e
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-12-26 19:16:10 +0900

    FIX: (kwordle) answer 서비스 가져오기

--- src/kwordle/kwordle.module.ts
+++ src/kwordle/kwordle.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { FIRST_KEYWORD } from 'src/util/consts/module-token.const';
 import { WebClientService } from 'src/util/web-client/web-client.service';
+import { AnswerService } from './answer/answer.service';
 import { KwordleResolver } from './kwordle.resolver';
 import { KwordleService } from './kwordle.service';
 
@@ -15,6 +16,7 @@ import { KwordleService } from './kwordle.service';
      * Services
      */
     KwordleService,
+    AnswerService,
     /**
      * 상수
      */
