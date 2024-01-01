e4685c0c96c2e8f4be710326a53bcc7299062193
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2024-01-01 22:48:54 +0900

    FIX: (answer) 특정 키워드 검색 후 해당 키워드의 설명을 분해한 후 검색해서 명사인 경우를 정답으로 가져올 수 있도록 로직 추가

--- src/kwordle/answer/answer.service.ts
+++ src/kwordle/answer/answer.service.ts
@@ -36,6 +36,60 @@ export class AnswerService implements OnModuleInit {
   }
 
   private async _getAnswer(): Promise<Answer> {
+    const key = this.configService.get('DICTIONARY_API_KEY');
+    const url = 'https://stdict.korean.go.kr/api/search.do';
+    const method = 'get';
+    const params: Record<string, unknown> = {
+      key,
+      type_search: 'search',
+      req_type: 'json',
+      q: this.answer?.word || '나무',
+    };
+
+    const result = await this.webClientService
+      .create(url)
+      .method(method)
+      .params(params)
+      .retrieve();
+
+    let answer: Answer;
+
+    const {
+      channel: { item: itemList },
+    } = result.rawBody as DICT_TYPE;
+
+    for await (const item of itemList) {
+      console.log(`item: ${item.word}`);
+      const definitionTokenList = item.sense.definition
+        .split(' ')
+        .map((per) => per.trim());
+      for await (const token of definitionTokenList) {
+        console.log(`token: ${token}`);
+        params.q = token;
+        const tokenSearchResult = await this.webClientService
+          .create(url)
+          .method(method)
+          .params(params)
+          .retrieve();
+
+        if (tokenSearchResult.rawBody === '') {
+          continue;
+        }
+
+        const {
+          channel: { item: itemListByToken },
+        } = tokenSearchResult.rawBody as DICT_TYPE;
+
+        for await (const tokenItem of itemListByToken) {
+          console.log(`tokenItem: ${tokenItem.word}`);
+
+          if (tokenItem.pos === '명사') {
+            answer = new Answer(tokenItem.word, tokenItem.sense.definition);
+          }
+        }
+      }
+    }
+
     return new Answer('나무', '나무');
   }
 }
